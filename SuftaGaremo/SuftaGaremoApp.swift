d5cb80827d78bc6212753dba56e19f34feabed2e
Giorgi Samkharadze <gsamkharadze51@gmail.com>
2024-06-22 01:20:53 +0400

    First commit

---
+++ SuftaGaremo/SuftaGaremoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuftaGaremoApp.swift
+//  SuftaGaremo
+//
+//  Created by Giorgi Samkharadze on 21.06.24.
+//
+
+import SwiftUI
+
+@main
+struct SuftaGaremoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
